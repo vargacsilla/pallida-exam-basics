c3eb59a18fc467454435f4557f36ece2af648673
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-06 12:03:00 +0200

    CandyShop: architecture created

---
+++ candyshop/CandyShop/CandyShop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyShop
+{
+    class CandyShop
+    {
+        int sugarStored;
+        int income;
+        List<Sweets> inventory;
+
+        public void MakeSweets()
+        {
+            //TODO
+        }
+
+        public void SellSweets()
+        {
+            //TODO
+        }
+
+        public void BuySugar()
+        {
+            //TODO
+        }
+
+        public void RaisePrice()
+        {
+            //TODO
+        }
+
+        public void PrintInfo()
+        {
+            //TODO
+        }
+    }
+}
