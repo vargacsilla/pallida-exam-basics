7929886e53f5c66bae49d4d4183f71938462b8d3
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-06 09:56:44 +0200

    feat(UniqueCharacters): adds input strings unique chars to list

---
+++ uniquechars/UniqueChars/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static List<char> UniqueCharacters(string inputString)
+        {
+            var charList = new List<char>();
+            foreach (var letter in inputString)
+            {
+                try
+                {
+                    int existingIndex = charList.IndexOf(letter);
+                    charList.RemoveAt(existingIndex);
+                }
+                catch
+                {
+                    charList.Add(letter);
+                }
+            }
+            return charList;
+        }
+    }
+}
