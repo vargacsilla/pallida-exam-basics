395de980fa9a82dea0225d13513466cfa0f546d4
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-06 10:39:10 +0200

    feat(UniqueaCharactersTests): 4 tests added

--- uniquechars/UniqueChars/UniqueCharactersTests.cs
+++ uniquechars/UniqueChars/UniqueCharactersTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace UniqueChars
 {
+    [TestFixture]
     class UniqueCharactersTests
     {
+        [Test]
+        public void UniqueCharactersTest_WhenDifferentLetters_ReturnSame()
+        {
+            string input = "abcd";
+            List<char> expected = new List<char>() { 'a', 'b', 'c', 'd' };
+
+            var output = Program.UniqueCharacters(input);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void UniqueCharactersTest_WhenDuplicateLetters_ReturnRest()
+        {
+            string input = "abbcd";
+            List<char> expected = new List<char>() { 'a', 'c', 'd' };
+
+            var output = Program.UniqueCharacters(input);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void UniqueCharactersTest_WhenNull_ReturnNull()
+        {
+            string input = null;
+            var expected = new List<char>();
+
+            var output = Program.UniqueCharacters(input);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void UniqueCharactersTest_WhenAllSame_ReturnEmpty()
+        {
+            string input = "aaaa";
+            var expected = new List<char>();
+
+            var output = Program.UniqueCharacters(input);
+
+            Assert.AreEqual(expected, output);
+        }
     }
 }
