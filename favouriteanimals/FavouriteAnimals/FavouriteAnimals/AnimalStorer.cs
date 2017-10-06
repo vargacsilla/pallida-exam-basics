52148091e98ae52614650ec6d859bae425454b5a
Csilla Varga <vargacsillamagda@gmail.com>
2017-10-06 10:58:54 +0200

    feat(AnimalStorer): AddAnimal() function

---
+++ favouriteanimals/FavouriteAnimals/FavouriteAnimals/AnimalStorer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteAnimals
+{
+    class AnimalStorer
+    {
+        List<string> animals;
+        string path = @"../../favourites.txt";
+
+        public void AddAnimal(string inputAnimal)
+        {
+            if (animals.Contains(inputAnimal))
+            {
+                return;
+            }
+            else
+            {
+                animals.Add(inputAnimal);
+                using (StreamWriter writer = File.AppendText(path))
+                {
+                    writer.WriteLine(inputAnimal);
+                }
+            }
+        }
+    }
+}
